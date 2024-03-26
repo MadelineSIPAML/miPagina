ad5b73038a7464fded30512bb0acf8c65206a1cd
MadelineSIPAML <162452139+MadelineSIPAML@users.noreply.github.com>
2024-03-26 11:28:11 -0300

    Add files via upload

---
+++ progamacion.js
@@ -0,0 +1,16 @@
+let nombre = "pablo" 
+var apellido = "tapia"
+
+let edad = 18
+var estatura = 1.74
+
+let conectado = true
+var desconectado = false 
+
+let arreglo = ["pablo" , "tapia" , 18 , 1.74 ]
+let objeto = {nombre : "pablo" , apellido : "tapia" , edad : 18 , estatura : 1.74 }
+
+
+
+
+
